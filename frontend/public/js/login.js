99a4f91a1760f5cd2d6e7c1730b73decd65a3dee
Rajiv-Stha <rajivnewar123@gmail.com>
2023-10-27 16:23:28 +0545

    signup and login js

---
+++ frontend/public/js/login.js
@@ -0,0 +1,35 @@
+
+
+const handleLogin = async (e) => {
+    e.preventDefault();
+  
+    const email = document.querySelector("#login_email");
+  
+    const password = document.querySelector("#login_password");
+  
+    const emailPayload = {
+      email: email.value,
+      password: password.value,
+    };
+    try {
+      
+      const { status, data } = await axios.post(
+        "https://shophub.onrender.com/api/auth/login",
+        emailPayload
+      );
+      email.value = "";
+      password.value = "";
+      if (status === 200) {
+        localStorage.setItem("user", JSON.stringify(data.message));
+        // await showToast("success", "logged in successfully");
+        localStorage.setItem("first", JSON.stringify(true));
+  
+        window.location.href = "http://127.0.0.1:5500/frontend/index.html";
+      }
+    } catch (error) {
+      console.log(error);
+      showToast("error", error?.response?.data?.message);
+    }
+  };
+  
+  document.querySelector("#loginForm").addEventListener("submit", handleLogin);
---
+++ frontend/public/js/signUp.js
@@ -0,0 +1,65 @@
+
+
+const handleSignup = async (e) => {
+    e.preventDefault();
+    console.log("inside form");
+    const username = document.querySelector("#signup_username");
+  
+    const email = document.querySelector("#signup_email");
+    const country = document.querySelector("#signup_select");
+    const password = document.querySelector("#signup_password");
+    const confirmPassword = document.querySelector("#signup_confirm_password");
+    const signupPayload = {
+      username: username.value,
+      email: email.value,
+      country: country.value,
+      password: password.value,
+      confirmPassword: confirmPassword.value,
+    };
+  
+    try {
+      if (signupPayload.password !== signupPayload.confirmPassword) {
+        // alert("password doesn't match");
+        return;
+      }
+      const { data, status } = await axios.post(
+        "https://shophub.onrender.com/api/auth/register",
+        signupPayload
+      );
+  
+      username.value = "";
+      email.value = "";
+      country.value = "";
+      password.value = "";
+      confirmPassword.value = "";
+      if (status === 200) {
+        setTimeout(() => {
+          window.location.href =
+            "http://127.0.0.1:5500/frontend/public/html/login.html";
+        }, 2000);
+        showToast("success", "successfully registered");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  const fetchCountry = async () => {
+    try {
+      const { data, status } = await axios.get(
+        "https://restcountries.com/v3.1/all"
+      );
+      console.log(data);
+      data.forEach((country) => {
+        document.querySelector(
+          "#signup_select"
+        ).innerHTML += ` <option value="${country.name.common}">${country.name.common}</option>`;
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  fetchCountry();
+  
+  document.querySelector("#signupForm").addEventListener("submit", handleSignup);
+  console.log(document.querySelector("#signupForm"));
