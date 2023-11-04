1b598a6d61277f0c2aa67e97bd083a4f310bdf61
santosh <santehero8@gmail.com>
2023-11-04 23:55:42 +0545

    formated product schema

--- frontend/public/js/home.js
+++ frontend/public/js/home.js
@@ -67,3 +67,113 @@ const handleAddToCart=(event,product) =>{
     let cartData = {...product,cartQuantity:1}
     addToCart(cartData);
 }
+const FetchAllMonitors=async()=>{
+
+
+    try {
+          const {data,status} =  await axios.get("https://ecommerce-dxp5.onrender.com/api/product?subCategory=Monitor")
+            data.message.forEach((product)=>{
+                
+
+
+                const productCardHtml = document.createElement("div");
+                productCardHtml.classList.add("newProducts_card");
+                productCardHtml.addEventListener("click",()=>{
+                    location.href=  `https://miralimammad.netlify.app/public/html/productDetail.html?productId=${product._id}`
+                })
+
+                productCardHtml.innerHTML = `
+                <p class="newProducts_stockInfo_para">✅ in stock</p>
+                <div class="newProducts_imgWrapper">
+                <img class="newProducts_Img" src=${product.image[0]}>
+                </div>
+                <div class="newProduct_review_box">
+                Reviews
+                </div>
+                <div class="newProduct_details">
+                <p>${product.name}</p>
+                <p>${product.desc}</p>
+                <p>All-In-One</p>
+                </div>
+                <div class="newProduct_priceBox">
+                <h2 class="newProduct_price">$${product.price}</h2>
+                </div>
+                `
+                const addToCartButton = document.createElement("button");
+                addToCartButton.className = "addToCart_btn";
+                addToCartButton.innerHTML = `
+                    <img src="./public/icons/cartIconBLue.png" alt="cart">
+                    <p>Add to Cart</p>
+                `;
+
+                addToCartButton.addEventListener("click",(e)=>handleAddToCart(e,product))
+                productCardHtml.append(addToCartButton)
+
+               
+
+
+
+                document.querySelector(".allMonitors").appendChild(productCardHtml);
+                
+            })
+
+    } catch (error) {
+        
+    }
+
+}
+const FetchAllLaptops=async()=>{
+
+    try {
+               const {data,status} =  await axios.get("https://ecommerce-dxp5.onrender.com/api/product?subCategory=Laptop")
+            data.message.forEach((product)=>{
+                
+
+
+                const productCardHtml = document.createElement("div");
+                productCardHtml.classList.add("newProducts_card");
+                productCardHtml.addEventListener("click",()=>{
+                    location.href=  `https://miralimammad.netlify.app/public/html/productDetail.html?productId=${product._id}`
+                })
+
+                productCardHtml.innerHTML = `
+                <p class="newProducts_stockInfo_para">✅ in stock</p>
+                <div class="newProducts_imgWrapper">
+                <img class="newProducts_Img" src=${product.image[0]}>
+                </div>
+                <div class="newProduct_review_box">
+                Reviews
+                </div>
+                <div class="newProduct_details">
+                <p>${product.name}</p>
+                <p>${product.desc}</p>
+                <p>All-In-One</p>
+                </div>
+                <div class="newProduct_priceBox">
+                <h2 class="newProduct_price">$${product.price}</h2>
+                </div>
+                `
+                const addToCartButton = document.createElement("button");
+                addToCartButton.className = "addToCart_btn";
+                addToCartButton.innerHTML = `
+                    <img src="./public/icons/cartIconBLue.png" alt="cart">
+                    <p>Add to Cart</p>
+                `;
+
+                addToCartButton.addEventListener("click",(e)=>handleAddToCart(e,product))
+                productCardHtml.append(addToCartButton)
+
+               
+
+
+
+                document.querySelector(".allLaptops").appendChild(productCardHtml);
+                
+            })
+    } catch (error) {
+        
+    }
+
+}
+FetchAllLaptops()
+FetchAllMonitors()
