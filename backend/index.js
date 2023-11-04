881e6a5e8163f199e1dbfe1c2e6dfd530f89d126
santosh <santehero8@gmail.com>
2023-11-04 19:24:12 +0545

    addeed a cors

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const session = require("express-session");
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
+    origin: ["http://127.0.0.1:5500", "http://localhost:5500","https://miralimammad.netlify.app/"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
