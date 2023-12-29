de4b1127add4db3dd6557f4d53ded7682837e760
Rajiv-Stha <rajivnewar123@gmail.com>
2023-12-29 22:41:51 +0545

    explaination about this project

--- frontend/public/js/cart.js
+++ frontend/public/js/cart.js
@@ -1,27 +1,36 @@
-let cartItem = getCartItems();
+let cartItem = getCartItems();//This line initializes a variable cartItem by calling the getCartItems function, presumably fetching the items in the user's shopping cart.
 
 const displayAllCarts = () => {
+  // Fetching cart items again (refreshing the cart)
   cartItem = getCartItems();
 
+  // Checking if the cart is empty
   if (!cartItem.length) {
+     // If empty, hide the shopping cart content and display a message for an empty cart
     document.querySelector(".shoppingCart_innerContent").style.display = "none";
     document.querySelector(".empty_shopping_cart_content").style.display =
       "flex";
   } else {
+     // If not empty, show the shopping cart content and hide the empty cart message
     document.querySelector(".shoppingCart_innerContent").style.display = "flex";
     document.querySelector(".empty_shopping_cart_content").style.display =
       "none";
   }
 
+    // Initializing total amount
   let totalAmount = 0;
 
+  // Updating "Buy Now" button text if the cart is empty
   if (!cartItem.length) {
     document.querySelector(".buyNowButton").innerText = "Your cart is empty";
   }
+    // Clearing the product list HTML
   document.querySelector(".shoppingCartProductList").innerHTML = "";
-
+  // Iterating through each item in the cart and updating the display
   cartItem.forEach((cart) => {
+      // Calculating the total amount
     totalAmount += cart.cartQuantity * Number(cart.price);
+      // Updating the product list HTML
     document.querySelector(".shoppingCartProductList").innerHTML += `
                                 <tr class="cart_product_item">
                                     <td >
@@ -52,6 +61,7 @@ const displayAllCarts = () => {
         `;
   });
 
+  // Displaying total amount and updating the display
   document.querySelector(
     ".summarySubTotalAmount"
   ).innerText = `$${totalAmount}`;
@@ -59,6 +69,7 @@ const displayAllCarts = () => {
     totalAmount + 10
   }`;
 
+   // Adding event listeners to the "remove" buttons for each product
   document.querySelectorAll(".removeCartButton").forEach((element) => {
     element.addEventListener("click", () => {
       const id = element.getAttribute("id");
@@ -70,17 +81,19 @@ const displayAllCarts = () => {
 };
 const handleBuyclick = (e) => {
   e.preventDefault();
+  // Checking if a user is logged in
   let user = fetchLoggedInUser();
 
   if (!user) {
     alert(" you  need to login first ");
     return;
   }
-
+ // Checking if the cart is empty
   if (!cartItem.length) {
     alert("Your cart is empty");
     return;
   }
+    // Saving shipping details to local storage and redirecting to the checkout page
   let shippingPayload = {
     address: document.querySelector("#shipping_address_input").value,
     number: document.querySelector("#shipping_phone_input").value,
@@ -103,7 +116,7 @@ document
     displayAllCarts();
     displayCartCount();
   });
-
+// Adjusting the display based on whether a user is logged in or not
 if (fetchLoggedInUser()) {
   document.querySelector(".shoppingCartLoginButton").style.display = "none";
   document.querySelector(".buyNowButton").style.display = "block";
@@ -111,3 +124,6 @@ if (fetchLoggedInUser()) {
   document.querySelector(".shoppingCartLoginButton").style.display = "block";
   document.querySelector(".buyNowButton").style.display = "none";
 }
+
+
+//This code manages the display of shopping cart items, allows users to remove items, and handles the process of initiating a purchase. The code also considers user authentication and updates the UI accordingly. 
